2a1fd5aca4bf63e8d560e5958895f3cb34ae6cc0
Adrien Morel <adrien.morel@me.com>
2015-03-23 12:13:36 -0400

    add a basic notification system for android wear

--- wear/src/main/java/com/davy/adrien/wildoo/MainActivity.java
+++ wear/src/main/java/com/davy/adrien/wildoo/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
+import android.support.v4.app.NotificationCompat.WearableExtender;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 public class MainActivity extends Activity {
 
@@ -20,5 +23,28 @@ public class MainActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+        int notificationId = 001;
+//        Build intent for notification content
+//            Intent viewIntent = new Intent(this, ViewEventActivity.class);
+//        viewIntent.putExtra(EXTRA_EVENT_ID, eventId);
+//        PendingIntent viewPendingIntent =
+//            PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+            new NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.ic_launcher)
+            .setContentTitle("hello world")
+            .setContentText("this is the text of the notification");
+            //.setContentIntent(viewPendingIntent);
+
+        // Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+            NotificationManagerCompat.from(this);
+
+        // Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+        finish();
     }
+
 }
